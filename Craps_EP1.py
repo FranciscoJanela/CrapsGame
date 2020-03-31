4c11cbcc19100403edc2f87cd91b3136c0df229e
FranciscoJanela <62965656+FranciscoJanela@users.noreply.github.com>
2020-03-31 20:31:43 -0300

    pass line bet, twelve e arranjos na fase point

--- Craps_EP1.py
+++ Craps_EP1.py
@@ -72,13 +72,17 @@ while fichas>0:
             aposta=int(input('o valor da aposta será: '))
             fichas-=aposta
             if tipo_de_aposta=='Pass Line Bet':
-                
+                resultado=pass_line_bet(soma_co,fichas,aposta,tipo_de_fase)
+                fichas=resultado[0]
+                tipo_de_fase=resultado[1]
+                Point=resultado[2]
+                aposta_point=resultado[3]
             elif tipo_de_aposta == 'Field':
                 fichas = field(soma_co, fichas, aposta)
             elif tipo_de_aposta == 'Any Craps':
                 fichas = any_craps(soma_co, fichas, aposta)
             elif tipo_de_aposta=='Twelve':
-                
+                fichas=twelve(soma_co,fichas,aposta)
             else:
                 print('resposta inválida, tente novamente')
         elif tipo_de_fase=='Point':
@@ -86,17 +90,30 @@ while fichas>0:
             dado2p = random.randint(1, 6)
             soma_p = dado1p + dado2p
             print('Está na fase Point,\ntem disponível os seguintes tipos de aposta')
-            print(' - Field\n - Any Craps\n - Twelve')
+            print(' - Field\n - Any Craps\n - Twelve\n - somente Point')
             print('(para escolher digite exatamente o nome do tipo de aposta)')
             tipo_de_aposta =input('a aposta será: ')
             valor_da_aposta=int(input('o valor da aposta será: '))
             fichas-=aposta
             if tipo_de_aposta=='Field':
                 fichas = field(soma_p, fichas, aposta)
+                resultado=pointBet(point,soma,fichas,aposta_point,tipo_de_fase)
+                fichas=resultado[0]
+                tipo_de_fase=resultado[1]
             elif tipo_de_aposta == 'Any Craps':
                 fichas = any_craps(soma_p, fichas, aposta)
+                resultado=pointBet(point,soma,fichas,aposta_point,tipo_de_fase)
+                fichas=resultado[0]
+                tipo_de_fase=resultado[1]
             elif tipo_de_aposta=='Twelve':
-
+                fichas=twelve(soma_p,fichas,aposta)
+                resultado=pointBet(point,soma,fichas,aposta_point,tipo_de_fase)
+                fichas=resultado[0]
+                tipo_de_fase=resultado[1]
+            elif tipo_de_aposta=='somente Point':
+                resultado=pointBet(point,soma,fichas,aposta_point,tipo_de_fase)
+                fichas=resultado[0]
+                tipo_de_fase=resultado[1]
             else:
                 print('resposta inválida, tente novamente')
             
