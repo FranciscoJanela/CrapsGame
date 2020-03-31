2ce4fb4dde8ab4be0f204fba2eab20c97c49dabb
nicolasmq20 <62630822+nicolasmq20@users.noreply.github.com>
2020-03-31 20:11:06 -0300

    def de field e any craps

--- Craps_EP1.py
+++ Craps_EP1.py
@@ -6,6 +6,26 @@ Craps!
 """
 import random
 
+def field(soma, fichas, aposta):
+    if soma == 5 or soma == 6 or soma == 7 or soma == 8:
+        fichas = fichas
+    elif soma == 3 or soma == 4 or soma == 9 or soma == 10 or soma == 11:
+        fichas += aposta*2
+    elif soma == 2:
+        fichas += aposta*3
+    elif soma == 12:
+        fichas += aposta*4
+    return fichas
+
+def any_craps(soma, fichas, aposta):
+    if soma == 2 or soma == 3 or soma == 12:
+        fichas += aposta*8
+    else:
+        fichas = fichas
+    return fichas
+
+
+
 fichas = int(imput('Quantas fichas você quer? '))
 tipo_de_fase=str('Come out')
 while fichas>0:
