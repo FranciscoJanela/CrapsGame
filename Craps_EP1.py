4b11ad3a802287d5c5b1ebdab58c3afdf32d48cf
FranciscoJanela <62965656+FranciscoJanela@users.noreply.github.com>
2020-03-31 18:51:42 -0300

    Update Craps_EP1.py

--- Craps_EP1.py
+++ Craps_EP1.py
@@ -5,13 +5,26 @@ Craps!
 @author: Francisco Janela & Nicolas Queiroga
 """
 import random
+
 fichas = int(imput('Quantas fichas você quer? '))
 tipo_de_fase=str('Come out')
 while fichas>0:
-    if tipo_de_fase=='Come out'
-        print('Você está na fase Come Out')
-        dado1 = random.randint(1, 6)
-        dado2 = random.randint(1, 6)
-        soma_co = dado1 + dado2
-        tipo_de_aposta =input(' ')
+    in_out=input('deseja apostar ou sair?\n(digite "apostar" ou "sair")\nResposta:')
+    if in_out=='apostar':
+        if tipo_de_fase=='Come out'
+            print('Você está na fase Come Out')
+            dado1co = random.randint(1, 6)
+            dado2co = random.randint(1, 6)
+            soma_co = dado1co + dado2co
+            print('Está na fase Come Out,\ntem disponível os seguintes tipos de aposta\n - Pass Line Bet\n')
+            tipo_de_aposta =input(' ')
+        elif tipo_de_fase=='Point':
+            dado1p = random.randint(1, 6)
+            dado2p = random.randint(1, 6)
+            somap = dado1p + dado2p
+    elif in_out=='sair':
+        break
+    else:
+        print('resposta inválida')
+    
         
