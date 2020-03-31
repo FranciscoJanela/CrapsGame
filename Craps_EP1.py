f69c1ff55e221bc6ba388c789f7e21613000a8b1
FranciscoJanela <62965656+FranciscoJanela@users.noreply.github.com>
2020-03-31 19:11:23 -0300

    input da fase Point

--- Craps_EP1.py
+++ Craps_EP1.py
@@ -18,9 +18,9 @@ while fichas>0:
             soma_co = dado1co + dado2co
             print('Está na fase Come Out,\ntem disponível os seguintes tipos de aposta')
             print(' - Pass Line Bet\n - Field\n - Any Craps\n - Twelve')
-            print('(para escolher digite exatamente o nome da aposta)')
+            print('(para escolher digite exatamente o nome do tipo de aposta)')
             tipo_de_aposta =input('a aposta será: ')
-            valor_da_aposta
+            valor_da_aposta=int(input('o valor da aposta será: '))
             if tipo_de_aposta=='Pass Line Bet':
                 if soma_co==7 or soma_co==11:
                     fichas+=
@@ -28,7 +28,7 @@ while fichas>0:
                     
                 else:
                     
-            if tipo_de_aposta == 'Field':
+            elif tipo_de_aposta == 'Field':
                 if soma_co == 5 or soma_co == 6 or soma_co == 7 or soma_co == 8:
                     fichas -= fichas
                 elif soma_co == 3 or soma_co == 4 or soma_co == 9 or soma_co == 10 or soma_co == 11:
@@ -41,6 +41,11 @@ while fichas>0:
             dado1p = random.randint(1, 6)
             dado2p = random.randint(1, 6)
             somap = dado1p + dado2p
+            print('Está na fase Point,\ntem disponível os seguintes tipos de aposta')
+            print(' - Field\n - Any Craps\n - Twelve')
+            print('(para escolher digite exatamente o nome do tipo de aposta)')
+            tipo_de_aposta =input('a aposta será: ')
+            valor_da_aposta=int(input('o valor da aposta será: '))
     elif in_out=='sair':
         break
     else:
