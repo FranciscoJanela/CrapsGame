b56c29e8f8b80863eac02cd8d17d05011e5989d7
FranciscoJanela <62965656+FranciscoJanela@users.noreply.github.com>
2020-03-31 18:38:25 -0300

    Inicio do programa

--- Craps_EP1.py
+++ Craps_EP1.py
@@ -4,15 +4,14 @@ Craps!
 
 @author: Francisco Janela & Nicolas Queiroga
 """
+import random
 fichas = int(imput('Quantas fichas você quer? '))
+tipo_de_fase=str('Come out')
 while fichas>0:
-    dado1 = random.randint(1, 6)
-    dado2 = random.randint(1, 6)
-    soma = dado1 + dado2
-    print('Você está na fase Come Out')
-    aposta = int(input('Qual é a sua aposta? '))
-    if soma == 7 or soma == 11:
-        fichas += aposta
-    elif soma == 2 or soma == 3 or soma == 12:
-        fichas -= aposta
-    else:
+    if tipo_de_fase=='Come out'
+        print('Você está na fase Come Out')
+        dado1 = random.randint(1, 6)
+        dado2 = random.randint(1, 6)
+        soma_co = dado1 + dado2
+        tipo_de_aposta =input(' ')
+        
