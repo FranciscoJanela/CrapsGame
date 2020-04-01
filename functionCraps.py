ade494917cd0a9770d524d31f370cb272dcf319b
nicolasmq20 <62630822+nicolasmq20@users.noreply.github.com>
2020-04-01 09:29:07 -0300

    def fase_point

--- functionCraps.py
+++ functionCraps.py
@@ -10,7 +10,7 @@ def come_out(fichas,tipo_de_fase):
   print('(para escolher digite exatamente o nome do tipo de aposta)')
   tipo_de_aposta =input('a aposta será: ')
   aposta=int(input('o valor da aposta será: '))
-  fichas-=aposta
+  fichas -= aposta
   if tipo_de_aposta=='Pass Line Bet':
       resultado=tA.pass_line_bet(soma_co,fichas,aposta,tipo_de_fase)
       fichas=resultado[0]
@@ -27,3 +27,36 @@ def come_out(fichas,tipo_de_fase):
       print('resposta inválida, tente novamente
   resultado=[fichas,tipo_de_fase,Point,aposta_point]
   return resultado
+
+def fase_point(fichas, tipo_de_fase, Point, aposta_point)           
+  dado1p = random.randint(1, 6)
+  dado2p = random.randint(1, 6)
+  soma_p = dado1p + dado2p
+  print('Está na fase Point,\ntem disponível os seguintes tipos de aposta')
+  print(' - Field\n - Any Craps\n - Twelve\n - somente Point')
+  print('(para escolher digite exatamente o nome do tipo de aposta)')
+  tipo_de_aposta =input('a aposta será: ')
+  valor_da_aposta=int(input('o valor da aposta será: '))
+  fichas-=aposta
+  if tipo_de_aposta=='Field':
+      fichas = field(soma_p, fichas, aposta)
+      resultado=pointBet(point,soma,fichas,aposta_point,tipo_de_fase)
+      fichas=resultado[0]
+      tipo_de_fase=resultado[1]
+  elif tipo_de_aposta == 'Any Craps':
+      fichas = any_craps(soma_p, fichas, aposta)
+      resultado=pointBet(point,soma,fichas,aposta_point,tipo_de_fase)
+      fichas=resultado[0]
+      tipo_de_fase=resultado[1]
+  elif tipo_de_aposta=='Twelve':
+      fichas=twelve(soma_p,fichas,aposta)
+      resultado=pointBet(point,soma,fichas,aposta_point,tipo_de_fase)
+      fichas=resultado[0]
+      tipo_de_fase=resultado[1]
+  elif tipo_de_aposta=='somente Point':
+      resultado=pointBet(point,soma,fichas,aposta_point,tipo_de_fase)
+      fichas=resultado[0]
+      tipo_de_fase=resultado[1]
+  else:
+      print('resposta inválida, tente novamente')
+  return fichas, tipo_de_fase
