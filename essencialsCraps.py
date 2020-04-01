286625b24ebb041873eab92bb214b308e5ff95fe
Francisco Janela <francisco.pinheiro.janela@gmail.com>
2020-04-01 10:51:33 -0300

    def print resposta da aposta

--- essencialsCraps.py
+++ essencialsCraps.py
@@ -5,4 +5,12 @@ def soma_dos_dados():
     dado2=random.randint(1,6)
     soma=dado1+dado2
     return soma
+
+def resposta_da_aposta(fichas,vitoria):
+    if vitoria:
+        print('parabéns! Sua aposta rendeu!\n você agora tem {0} fichas.'.format(fichas))
+    else:
+        print('Que pena... Você perdeu a aposta.\n você agora tem {0} fichas.'.format(fichas))
+    return vitoria
+
     
