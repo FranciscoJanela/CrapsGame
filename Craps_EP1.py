71d833ec2bee53fce6702d2073236a3d7fe23e4d
FranciscoJanela <62965656+FranciscoJanela@users.noreply.github.com>
2020-04-01 09:05:39 -0300

    incerção da biblioteca - p1

--- Craps_EP1.py
+++ Craps_EP1.py
@@ -5,54 +5,7 @@ Craps!
 @author: Francisco Janela & Nicolas Queiroga
 """
 import random
-
-def pass_line_bet(soma,fichas,aposta,tipo_de_fase):
-    if soma==7 or soma==11:
-        fichas+=aposta*2
-    elif soma==2 or soma==3 or soma==12:
-        fichas=fichas
-    else:
-        tipo_de_fase='Point'
-        fichas=ficha
-        aposta_point=aposta
-        point=soma
-    resultado=[fichas,tipo_de_fase,point,aposta_point]
-    return resultado
-
-def pointBet(point,soma,fichas,aposta_point,tipo_de_fase):
-    if soma==point:
-        fichas+=2*aposta_point
-        tipo_de_fase='Come out'
-    elif soma==7:
-        fichas=fichas
-        tipo_de_fase='Come out'
-    resultado=[fichas,tipo_de_fase]
-    return resultado
-
-def field(soma, fichas, aposta):
-    if soma == 5 or soma == 6 or soma == 7 or soma == 8:
-        fichas = fichas
-    elif soma == 3 or soma == 4 or soma == 9 or soma == 10 or soma == 11:
-        fichas += aposta*2
-    elif soma == 2:
-        fichas += aposta*3
-    elif soma == 12:
-        fichas += aposta*4
-    return fichas
-
-def any_craps(soma, fichas, aposta):
-    if soma == 2 or soma == 3 or soma == 12:
-        fichas += aposta*8
-    else:
-        fichas = fichas
-    return fichas
-
-def twelve(soma,fichas,aposta):
-    if soma==12:
-        fichas+=aposta*31
-    else:
-        fichas=fichas
-    return fichas
+import functionCraps.py as fC
 
 fichas = int(input('Quantas fichas você quer? '))
 point=0
@@ -62,30 +15,9 @@ while fichas>0:
     in_out=input('deseja apostar ou sair?\n(digite "apostar" ou "sair")\nResposta:')
     if in_out=='apostar':
         if tipo_de_fase=='Come out':
-            print('Você está na fase Come Out')
-            dado1co = random.randint(1, 6)
-            dado2co = random.randint(1, 6)
-            soma_co = dado1co + dado2co
-            print('Está na fase Come Out,\ntem disponível os seguintes tipos de aposta')
-            print(' - Pass Line Bet\n - Field\n - Any Craps\n - Twelve')
-            print('(para escolher digite exatamente o nome do tipo de aposta)')
-            tipo_de_aposta =input('a aposta será: ')
-            aposta=int(input('o valor da aposta será: '))
-            fichas-=aposta
-            if tipo_de_aposta=='Pass Line Bet':
-                resultado=pass_line_bet(soma_co,fichas,aposta,tipo_de_fase)
-                fichas=resultado[0]
-                tipo_de_fase=resultado[1]
-                Point=resultado[2]
-                aposta_point=resultado[3]
-            elif tipo_de_aposta == 'Field':
-                fichas = field(soma_co, fichas, aposta)
-            elif tipo_de_aposta == 'Any Craps':
-                fichas = any_craps(soma_co, fichas, aposta)
-            elif tipo_de_aposta=='Twelve':
-                fichas=twelve(soma_co,fichas,aposta)
-            else:
-                print('resposta inválida, tente novamente')
+            resultado_come_out=fC.come_out(fichas,tipo_de_fase)
+            fichas=resultado_come_out[0]
+            tipo_de_fase=resultado_come_out[1]
         elif tipo_de_fase=='Point':
             dado1p = random.randint(1, 6)
             dado2p = random.randint(1, 6)
