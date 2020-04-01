47ce9856c43ff9ba9729273d65040db694def6c4
FranciscoJanela <62965656+FranciscoJanela@users.noreply.github.com>
2020-04-01 09:02:05 -0300

    def come out

--- functionCraps.py
+++ functionCraps.py
@@ -1,1 +1,25 @@
 def come_out():
+  print('Você está na fase Come Out')
+  dado1co = random.randint(1, 6)
+  dado2co = random.randint(1, 6)
+  soma_co = dado1co + dado2co
+  print('Está na fase Come Out,\ntem disponível os seguintes tipos de aposta')
+  print(' - Pass Line Bet\n - Field\n - Any Craps\n - Twelve')
+  print('(para escolher digite exatamente o nome do tipo de aposta)')
+  tipo_de_aposta =input('a aposta será: ')
+  aposta=int(input('o valor da aposta será: '))
+  fichas-=aposta
+  if tipo_de_aposta=='Pass Line Bet':
+      resultado=pass_line_bet(soma_co,fichas,aposta,tipo_de_fase)
+      fichas=resultado[0]
+      tipo_de_fase=resultado[1]
+      Point=resultado[2]
+      aposta_point=resultado[3]
+  elif tipo_de_aposta == 'Field':
+      fichas = field(soma_co, fichas, aposta)
+  elif tipo_de_aposta == 'Any Craps':
+      fichas = any_craps(soma_co, fichas, aposta)
+  elif tipo_de_aposta=='Twelve':
+      fichas=twelve(soma_co,fichas,aposta)
+  else:
+      print('resposta inválida, tente novamente'
