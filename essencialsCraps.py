ce2e7b57bd58f2c3c45c9c0cdb51d1b9b53b2f28
Francisco Janela <francisco.pinheiro.janela@gmail.com>
2020-04-01 17:51:40 -0300

    modificações nos prints do resultdo das apostas

--- essencialsCraps.py
+++ essencialsCraps.py
@@ -8,18 +8,24 @@ def soma_dos_dados():
 
 def resposta_da_aposta(fichas,vitoria,tipo_de_aposta):
     if vitoria==True:
-        print('Parabéns! Sua aposta em {0} rendeu!\nVocê agora tem {1} fichas.'.format(tipo_de_aposta,fichas))
+        print('Parabéns!\nSua aposta em {0} rendeu!')
+        print('Você agora tem {1} fichas.'.format(tipo_de_aposta,fichas)')
     elif vitoria=='point':
-        print('Você foi para a fase de Point! Sua aposta ainda continua valendo.\nVocê agora tem {0} fichas.'.format(fichas))
+        print('Você foi para a fase de Point!\nSua aposta ainda continua valendo.')
+        print('Você agora tem {0} fichas.'.format(fichas))
     elif vitoria==False:
-        print('Que pena... Você perdeu a aposta em {0}.\nVocê agora tem {1} fichas.'.format(tipo_de_aposta,fichas))
+        print('Que pena...\nVocê perdeu a aposta em {0}.')
+        print('Você agora tem {1} fichas.'.format(tipo_de_aposta,fichas))
     else:
         if vitoria=='point vitoria':
-            print('Você ganhou no Point!\nvoltará para a fase Come Out, parabéns!\nVocê agora tem {0} fichas.'.format(fichas))
+            print('Você ganhou no Point!\nvoltará para a fase Come Out, parabéns!')
+            print(Você agora tem {0} fichas.'.format(fichas))
         elif vitoria=='point mantem':
-            print('Você se mantem na fase Point.\nContinua com {0} fichas.'.format(fichas))
+            print('Você se mantem na fase Point.')
+            print('Continua com {0} fichas.'.format(fichas))
         else:
-            print('Que pena, você perdeu no Point...\nVoltará para a fase Come Out.\nVocê agora tem {0} fichas.'.format(fichas))
+            print('Que pena, você perdeu no Point...\nVoltará para a fase Come Out.')
+            print('Você agora tem {0} fichas.'.format(fichas))
     fim_resposta='***'
     return fim_resposta
 
